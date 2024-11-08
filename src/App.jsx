bae8270db33eff83a6c9b967e6331ed7377dfe11
CapsE <larskrafft@yahoo.de>
2024-11-08 21:25:24 +0100

    Adds story examples to the game

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,44 @@ import {useState} from 'react'
 import evilCowboy from './assets/evil-cowboy.png';
 import styles from './App.module.css';
 import {ProgressIcon} from './components/progress-icon';
+import {observer} from 'mobx-react-lite';
 
 import Money from './icons/money-stack.svg?react';
 import Law from './icons/law-star.svg?react';
 import Staff from './icons/person.svg?react';
 import Fame from './icons/star-struck.svg?react';
 
-function App() {
-    const [count, setCount] = useState(0)
+import {state} from './state';
+import story from './story.json';
+
+const App = observer(() => {
+    const active = story[state.active];
+
+    const handleYes = () => {
+        state.updateValues(active.yes);
+        state.next();
+    };
+
+    const handleNo = () => {
+        state.updateValues(active.no);
+        state.next();
+    }
 
     return (
         <>
-            <h1>Bottleneck Bar</h1>
+            <h1>{active.title}</h1>
             <img className={styles.image} src={evilCowboy} />
-            <p>A stranger enters the saloon demanding a free drink while making sure to display the polished sixshooter at his hip.</p>
+            <p>{active.description}</p>
+            <button onClick={handleYes}>Yes</button>
+            <button onClick={handleNo}>No</button>
             <div className={styles.iconWrapper}>
-                <ProgressIcon value={25} icon={<Money />} />
-                <ProgressIcon value={50} icon={<Law />} />
-                <ProgressIcon value={75} icon={<Staff />} />
-                <ProgressIcon value={100} icon={<Fame />} />
+                <ProgressIcon value={state.money} icon={<Money />} />
+                <ProgressIcon value={state.law} icon={<Law />} />
+                <ProgressIcon value={state.staff} icon={<Staff />} />
+                <ProgressIcon value={state.fame} icon={<Fame />} />
             </div>
         </>
     )
-}
+});
 
 export default App
--- src/state.js
+++ src/state.js
@@ -1,13 +1,28 @@
-import { makeAutoObservable } from "mobx"
+import { makeAutoObservable } from "mobx";
+import story from './story.json';
 
 class State {
     constructor() {
+        this.active = 0;
         this.law = 50;
         this.money = 50;
         this.fame = 50;
         this.staff = 50;
 
         makeAutoObservable(this);
+    }
+
+    updateValues(values){
+        Object.keys(values).forEach((key) => {
+            this[key] += values[key];
+        });
+    }
+
+    next() {
+        this.active++;
+        if(this.active >= story.length) {
+            this.active = 0;
+        }
     }
 }
 
